4e5869593e11e5ca850a7612a951a80f956e3b6a
dallinjared <dallin.jared14@gmail.com>
2023-03-24 18:37:13 -0600

    Initial Commit

---
+++ Banner/BannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BannerApp.swift
+//  Banner
+//
+//  Created by Dallin Jared on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
